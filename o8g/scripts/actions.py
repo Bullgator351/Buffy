3e2c2b16c7e5c3d1accf316490c149347faa8768
Bullgator351 <lurking.gator@hotmail.com>
2019-04-24 18:35:05 -0500

    Lots of stuff

--- o8g/scripts/actions.py
+++ o8g/scripts/actions.py
@@ -2,6 +2,13 @@
 DarkLightToken = ("DarkLightToken", "252af8c6-f383-46f9-ae8b-f2f302bdf6b0")
 CourageToken = ("CourageToken", "c5be15ab-f1a6-442c-aba1-c64ccc14961b")
 
+# Add global debug
+#
+# Need to define x and y coordinates of the upper left corner of every pile on the table
+# Like bigbadx = this and bigbady = that
+# Library and Sunnydale will need to adapt code from LotR that auto spaces the cards out
+
+# Probably table card action but need to compare with flipcard
 
 def flip(card, x = 0, y = 0):
     mute()
@@ -16,9 +23,33 @@ def flip(card, x = 0, y = 0):
 # Table group actions
 #---------------------------------------------------------------------------
 
-def createDarkLightToken(group, x=0, y=0):
-	moveDarkLightToken(x, y)
+# (not working)
+# def createDarkLightToken(group, x=0, y=0):
+# 	moveDarkLightToken(x, y)
+
+def moveCard(model, x, y):
+	for c in table:
+		if c.model == model:
+			c.moveToTable(x, y)
+			return c
+	return table.create(model, x, y)
 	
+def moveDarkLightToken(x=0, y=0):
+	return moveCard("252af8c6-f383-46f9-ae8b-f2f302bdf6b0", x, y)
+	
+def getDarkLightToken():
+	for c in table:
+		if c.model == "252af8c6-f383-46f9-ae8b-f2f302bdf6b0":
+			return c
+	return None
+
+def getCourageToken():
+	for c in table:
+		if c.model == "c5be15ab-f1a6-442c-aba1-c64ccc14961b":
+			return c
+	return None
+
+
 def flipCoin(group, x = 0, y = 0):
     mute()
     n = rnd(1, 2)
@@ -48,6 +79,9 @@ def flipcard(card, x = 0, y = 0):
 
 	cardx, cardy = card.position
 	
+# Need playersetup, addencounter or createcard, addresource, discard, something adapted to KO
+
+	
 #------------------------------------------------------------------------------
 # Hand Actions
 #------------------------------------------------------------------------------
@@ -58,6 +92,8 @@ def randomDiscard(group):
 	if card is None: return
 	notify("{} randomly discards '{}'.".format(me, card))
 	card.moveTo(me.piles['Discard Pile'])
+	
+# Need discard and something adapted for KO
 
 #------------------------------------------------------------------------------
 # Pile Actions
@@ -69,3 +105,6 @@ def shuffle(group):
 		update()
 		group.shuffle()
 		notify("{} shuffles {}".format(me, group.name))
+		
+# Need something to track victory conditions - ideally put the number in the global hand display
+# Also tract Dark/Light in hand display
